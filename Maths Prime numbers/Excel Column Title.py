d0751ec61acba9e6663441f06f53c262f3a1329d
vipinsaini27 <56450662+vipinsaini27@users.noreply.github.com>
2022-01-21 21:47:12 +0530

    Excel Column Title Question Solved

--- Maths Prime numbers/Excel Column Title.py
+++ Maths Prime numbers/Excel Column Title.py
@@ -54,3 +54,28 @@ Explanation 2:
     27 -> AA,
     28 -> AB
 '''
+
+
+
+class Solution:
+    c = {1: "A", 2: "B", 3: "C", 4: "D", 5: "E", 6: "F", 7: "G", 8: "H", 9: "I", 10:"J",
+         11: "K", 12: "L", 13: "M", 14: "N", 15: "O", 16: "P", 17: "Q", 18: "R", 19: "S",
+         20: "T", 21: "U", 22: "V", 23: "W", 24: "X", 25: "Y", 26: "Z"}
+
+    def convertToTitle(self, A):
+        ans = ""
+        while A:
+            p = A % 26
+            if p == 0:
+                ans = "Z" + ans
+                A = (A // 26) - 1
+            else:
+                ans = self.c[p] + ans
+                A = A // 26
+
+        return ans
+
+
+A = 980089
+ans = Solution().convertToTitle(A)
+print(ans)
