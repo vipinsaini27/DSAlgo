de9f84f93e194199fa9b7c6964d69af047c84b82
vipinsaini27 <56450662+vipinsaini27@users.noreply.github.com>
2022-03-01 22:03:20 +0530

    Sample

--- Day 39 - Sorting 1/Inversion count in an array.py
+++ Day 39 - Sorting 1/Inversion count in an array.py
@@ -40,18 +40,60 @@ Explanation 2:
  No inversions.
 """
 
+"""
+Solution Approach
+Algorithm :
+Traverse through the array from start to end
+For every element find the count of elements smaller than the current number upto that index using another loop.
+Sum up the count of inversion for every index.
+Print the count of inversions.
+"""
+
 class Solution:
 
-    def solve(self, A):
-        ans = 0
-        m = 10**9 + 7
-        for i in range(len(A)):
-            for j in range(i+1, len(A)):
-                if A[i] > A[j]:
-                    ans = (ans + 1) % m
+    def mSort(self, A, ans):
+        if len(A) < 2:
+            return ans
+
+        m = len(A) // 2
+        lArr = A[:m]
+        rArr = A[m:]
+        ans = self.mSort(lArr, ans)
+        ans = self.mSort(rArr, ans)
+
+        flag = False
+        i, j = len(lArr) - 1, len(rArr) - 1
+        while i >= 0 and j >= 0:
+            if lArr[i] > rArr[j]:
+                ans = (ans + j + 1) % (10**9 + 7)
+                i -= 1
+            else:
+                j -= 1
+
+        i, j = 0, 0
+        k = 0
+        while i < len(lArr) and j < len(rArr):
+            if lArr[i] >= rArr[j]:
+                A[k] = rArr[j]
+                j += 1
+            else:
+                A[k] = lArr[i]
+                i += 1
+            k += 1
+
+        while i < len(lArr):
+            A[k] = lArr[i]
+            i += 1
+            k += 1
+
+        while j < len(rArr):
+            A[k] = rArr[j]
+            j += 1
+            k += 1
 
         return ans
 
-A = [1, 2, 3]
-ans = Solution().solve(A)
-print(ans)
+    def solve(self, A):
+        ans = self.mSort(A, 0)
+
+        return ans
